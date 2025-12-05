0ec2a97e3d2a13a9da45389aab74d14460c2c60d
Puggio77 <147837226+Puggio77@users.noreply.github.com>
2025-12-05 12:31:48 +0100

    initial commit

---
+++ DinnerParty/DinnerParty/DinnerPartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinnerPartyApp.swift
+//  DinnerParty
+//
+//  Created by Riccardo Puggioni on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DinnerPartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
