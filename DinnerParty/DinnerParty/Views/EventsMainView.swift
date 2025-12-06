3cb02ac6aba890addc28a6f2681dc1c181b2839e
Aleksandra <aleksandra.stupiec@gmail.com>
2025-12-06 21:20:54 +0100

    main view abd new event init

---
+++ DinnerParty/DinnerParty/Views/EventsMainView.swift
@@ -0,0 +1,18 @@
+//
+//  EventsMainView.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 05/12/25.
+//
+
+import SwiftUI
+
+struct EventsMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EventsMainView()
+}
