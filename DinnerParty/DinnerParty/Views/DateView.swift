513b398eb75de37b5b8f9a388b3b2721503a8c04
Aleksandra <aleksandra.stupiec@gmail.com>
2025-12-09 12:36:22 +0100

    date view and sheet with the view added

---
+++ DinnerParty/DinnerParty/Views/DateView.swift
@@ -0,0 +1,18 @@
+//
+//  DateView.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 09/12/25.
+//
+
+import SwiftUI
+
+struct DateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateView()
+}
