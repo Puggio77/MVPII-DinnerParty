e9b391c22c82db114d8faed387de1d71d9ee2a08
Aleksandra <aleksandra.stupiec@gmail.com>
2025-12-09 12:57:25 +0100

    Course stepper added

---
+++ DinnerParty/DinnerParty/Views/CourseStepperView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseStepperView.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 09/12/25.
+//
+
+import SwiftUI
+
+struct CourseStepperView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseStepperView()
+}
