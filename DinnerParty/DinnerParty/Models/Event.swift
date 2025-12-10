f647c346d082f490c1be27f715f530d4be2656ff
Aleksandra <aleksandra.stupiec@gmail.com>
2025-12-10 12:39:34 +0100

    model creation

---
+++ DinnerParty/DinnerParty/Models/Event.swift
@@ -0,0 +1,9 @@
+//
+//  EventModel.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 10/12/25.
+//
+
+import Foundation
+
---
+++ DinnerParty/DinnerParty/ViewModel/EventViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EventViewModel.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 10/12/25.
+//
+
+import Foundation
