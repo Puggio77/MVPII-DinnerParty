5eaa838b7c79108de230699d677f158919ec5566
Aleksandra <aleksandra.stupiec@gmail.com>
2025-12-14 23:57:16 +0100

    event model, date problem resolved

---
+++ DinnerParty/DinnerParty/Models/Users.swift
@@ -0,0 +1,8 @@
+//
+//  Users.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 12/12/25.
+//
+
+import Foundation
---
+++ DinnerParty/DinnerParty/Views/DetailEventView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEventView.swift
+//  DinnerParty
+//
+//  Created by Aleksandra Stupiec on 12/12/25.
+//
+
+import SwiftUI
+
+struct DetailEventView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEventView()
+}
